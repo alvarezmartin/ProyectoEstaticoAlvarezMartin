561a7ce942ca665df212b451122edacf6666ca8f
alvarezmartin <tinchoalvarez03@hotmail.com>
2023-09-03 15:32:16 -0300

    Se agrega el proyecto

---
+++ plantilla.js
@@ -0,0 +1,347 @@
+const btnArmani = document.querySelector("#btnArmani")
+const btnCerrar = document.querySelector("#btnCerrarArmani")
+const modalArmani = document.querySelector("#ventanaArmani")
+
+btnArmani.addEventListener("click",()=>{
+    modalArmani.showModal();
+})
+
+btnCerrar.addEventListener("click",()=>{
+    modalArmani.close();
+})
+
+const btnCenturion = document.querySelector("#btnCenturion")
+const btnCerrar2 = document.querySelector("#btnCerrarCenturion")
+const modalCenturion = document.querySelector("#ventanaCenturion")
+
+btnCenturion.addEventListener("click",()=>{
+    modalCenturion.showModal();
+})
+
+btnCerrar2.addEventListener("click",()=>{
+    modalCenturion.close();
+})
+
+const btnLavagnino = document.querySelector("#btnLavagnino")
+const btnCerrar3 = document.querySelector("#btnCerrarLavagnino")
+const modalLavagnino = document.querySelector("#ventanaLavagnino")
+
+btnLavagnino.addEventListener("click",()=>{
+    modalLavagnino.showModal();
+})
+
+btnCerrar3.addEventListener("click",()=>{
+    modalLavagnino.close();
+})
+
+const btnFmori = document.querySelector("#btnFmori")
+const btnCerrar4 = document.querySelector("#btnCerrarFmori")
+const modalFmori = document.querySelector("#ventanaFmori")
+
+btnFmori.addEventListener("click",()=>{
+    modalFmori.showModal();
+})
+
+btnCerrar4.addEventListener("click",()=>{
+    modalFmori.close();
+})
+
+const btnMaidana = document.querySelector("#btnMaidana")
+const btnCerrar5 = document.querySelector("#btnCerrarMaidana")
+const modalMaidana = document.querySelector("#ventanaMaidana")
+
+btnMaidana.addEventListener("click",()=>{
+    modalMaidana.showModal();
+})
+
+btnCerrar5.addEventListener("click",()=>{
+    modalMaidana.close();
+})
+
+const btnMartinez = document.querySelector("#btnMartinez")
+const btnCerrar6 = document.querySelector("#btnCerrarMartinez")
+const modalMartinez = document.querySelector("#ventanaMartinez")
+
+btnMartinez.addEventListener("click",()=>{
+    modalMartinez.showModal();
+})
+
+btnCerrar6.addEventListener("click",()=>{
+    modalMartinez.close();
+})
+
+const btnEdiaz = document.querySelector("#btnEdiaz")
+const btnCerrar7 = document.querySelector("#btnCerrarEdiaz")
+const modalEdiaz = document.querySelector("#ventanaEdiaz")
+
+btnEdiaz.addEventListener("click",()=>{
+    modalEdiaz.showModal()
+})
+
+btnCerrar7.addEventListener("click",()=>{
+    ventanaEdiaz.close()
+})
+
+const btnPirez = document.querySelector("#btnPirez")
+const btnCerrar8 = document.querySelector("#btnCerrarPirez")
+const modalPirez = document.querySelector("#ventanaPirez")
+
+btnPirez.addEventListener("click",()=>{
+    modalPirez.showModal();
+})
+
+btnCerrar8.addEventListener("click",()=>{
+    modalPirez.close();
+})
+
+const btnHerrera = document.querySelector("#btnHerrera")
+const btnCerrar9 = document.querySelector("#btnCerrarHerrera")
+const modalHerrera = document.querySelector("#ventanaHerrera")
+
+btnHerrera.addEventListener("click",()=>{
+    modalHerrera.showModal();
+})
+
+btnCerrar9.addEventListener("click",()=>{
+    modalHerrera.close();
+})
+
+const btnPdiaz = document.querySelector("#btnPdiaz")
+const btnCerrar10 = document.querySelector("#btnCerrarPdiaz")
+const modalPdiaz = document.querySelector("#ventanaPdiaz")
+
+btnPdiaz.addEventListener("click",()=>{
+    modalPdiaz.showModal();
+})
+
+btnCerrar10.addEventListener("click",()=>{
+    modalPdiaz.close();
+})
+
+const btnCasco = document.querySelector("#btnCasco")
+const btnCerrar11 = document.querySelector("#btnCerrarCasco")
+const modalCasco = document.querySelector("#ventanaCasco")
+
+btnCasco.addEventListener("click",()=>{
+    modalCasco.showModal();
+})
+
+btnCerrar11.addEventListener("click",()=>{
+    modalCasco.close();
+})
+
+const btnMammana = document.querySelector("#btnMammana")
+const btnCerrar12 = document.querySelector("#btnCerrarMammana")
+const modalMammana = document.querySelector("#ventanaMammana")
+
+btnMammana.addEventListener("click",()=>{
+    modalMammana.showModal();
+})
+
+btnCerrar12.addEventListener("click",()=>{
+    modalMammana.close()
+})
+
+const btnZuculini = document.querySelector("#btnZuculini")
+const btnCerrar13 = document.querySelector("#btnCerrarZuculini")
+const modalZuculini = document.querySelector("#ventanaZuculini")
+
+btnZuculini.addEventListener("click",()=>{
+    modalZuculini.showModal();
+})
+
+btnCerrar13.addEventListener("click",()=>{
+    modalZuculini.close();
+})
+
+const btnPalavecino = document.querySelector("#btnPalavecino")
+const btnCerrar14 = document.querySelector("#btnCerrarPalavecino")
+const modalPalavecino = document.querySelector("#ventanaPalavecino")
+
+btnPalavecino.addEventListener("click",()=>{
+    modalPalavecino.showModal();
+})
+
+btnCerrar14.addEventListener("click",()=>{
+    modalPalavecino.close();
+})
+
+const btnFernandez = document.querySelector("#btnFernandez")
+const btnCerrar15 = document.querySelector("#btnCerrarFernandez")
+const modalFernandez = document.querySelector("#ventanaFernandez")
+
+btnFernandez.addEventListener("click",()=>{
+    modalFernandez.showModal();
+})
+
+btnCerrar15.addEventListener("click",()=>{
+    modalFernandez.close();
+})
+
+const btnDLC = document.querySelector("#btnDLC")
+const btnCerrar16 = document.querySelector("#btnCerrarDLC")
+const modalDLC = document.querySelector("#ventanaDLC")
+
+btnDLC.addEventListener("click",()=>{
+    modalDLC.showModal();
+})
+
+btnCerrar16.addEventListener("click",()=>{
+    modalDLC.close();
+})
+
+const btnBarco = document.querySelector("#btnBarco")
+const btnCerrar17 = document.querySelector("#btnCerrarBarco")
+const modalBarco = document.querySelector("#ventanaBarco")
+
+btnBarco.addEventListener("click",()=>{
+    modalBarco.showModal();
+})
+
+btnCerrar17.addEventListener("click",()=>{
+    modalBarco.close();
+})
+
+const btnKranevitter = document.querySelector("#btnKranevitter")
+const btnCerrar18 = document.querySelector("#btnCerrarKranevitter")
+const modalKranevitter = document.querySelector("#ventanaKranevitter")
+
+btnKranevitter.addEventListener("click",()=>{
+    modalKranevitter.showModal();
+})
+
+btnCerrar18.addEventListener("click",()=>{
+    modalKranevitter.close();
+})
+
+const btnPerez = document.querySelector("#btnPerez")
+const btnCerrar19 = document.querySelector("#btnCerrarPerez")
+const modalPerez = document.querySelector("#ventanaPerez")
+
+btnPerez.addEventListener("click",()=>{
+    modalPerez.showModal();
+})
+
+btnCerrar19.addEventListener("click",()=>{
+    modalPerez.close();
+})
+
+const btnAliendro = document.querySelector("#btnAliendro")
+const btnCerrar20 = document.querySelector("#btnCerrarAliendro")
+const modalAliendro = document.querySelector("#ventanaAliendro")
+
+btnAliendro.addEventListener("click",()=>{
+    modalAliendro.showModal();
+})
+
+btnCerrar20.addEventListener("click",()=>{
+    modalAliendro.close();
+})
+
+const btnSimon = document.querySelector("#btnSimon")
+const btnCerrar21 = document.querySelector("#btnCerrarSimon")
+const modalSimon = document.querySelector("#ventanaSimon")
+
+btnSimon.addEventListener("click",()=>{
+    modalSimon.showModal();
+})
+
+btnCerrar21.addEventListener("click",()=>{
+    modalSimon.close();
+})
+
+const btnColidio = document.querySelector("#btnColidio")
+const btnCerrar22 = document.querySelector("#btnCerrarColidio")
+const modalColidio = document.querySelector("#ventanaColidio")
+
+btnColidio.addEventListener("click",()=>{
+    modalColidio.showModal();
+})
+
+btnCerrar22.addEventListener("click",()=>{
+    modalColidio.close();
+})
+
+const btnSuarez = document.querySelector("#btnSuarez")
+const btnCerrar23 = document.querySelector("#btnCerrarSuarez")
+const modalSuarez = document.querySelector("#ventanaSuarez")
+
+btnSuarez.addEventListener("click",()=>{
+    modalSuarez.showModal();
+})
+
+btnCerrar23.addEventListener("click",()=>{
+    modalSuarez.close();
+})
+
+const btnBorja = document.querySelector("#btnBorja")
+const btnCerrar24 = document.querySelector("#btnCerrarBorja")
+const modalBorja = document.querySelector("#ventanaBorja")
+
+btnBorja.addEventListener("click",()=>{
+    modalBorja.showModal();
+})
+
+btnCerrar24.addEventListener("click",()=>{
+    modalBorja.close();
+})
+
+const btnRondon = document.querySelector("#btnRondon")
+const btnCerrar25 = document.querySelector("#btnCerrarRondon")
+const modalRondon = document.querySelector("#ventanaRondon")
+
+btnRondon.addEventListener("click",()=>{
+    modalRondon.showModal();
+})
+
+btnCerrar25.addEventListener("click",()=>{
+    modalRondon.close();
+})
+
+const btnSolari = document.querySelector("#btnSolari")
+const btnCerrar26 = document.querySelector("#btnCerrarSolari")
+const modalSolari = document.querySelector("#ventanaSolari")
+
+btnSolari.addEventListener("click",()=>{
+    ventanaSolari.showModal();
+})
+
+btnCerrar26.addEventListener("click",()=>{
+    ventanaSolari.close();
+})
+
+const btnDemichelis = document.querySelector("#btnDemichelis")
+const btnCerrar27 = document.querySelector("#btnCerrarDemichelis")
+const modalDemichelis = document.querySelector("#ventanaDemichelis")
+
+btnDemichelis.addEventListener("click",()=>{
+    modalDemichelis.showModal();
+})
+
+btnCerrar27.addEventListener("click",()=>{
+    modalDemichelis.close();
+})
+
+const btnPinolax = document.querySelector("#btnPinola")
+const btnCerrar29 = document.querySelector("#btnCerrarPinola")
+const modalPinola = document.querySelector("#ventanaPinola")
+
+btnPinola.addEventListener("click",()=>{
+    modalPinola.showModal();
+})
+
+btnCerrar29.addEventListener("click",()=>{
+    modalPinola.close();
+})
+
+const btnLux = document.querySelector("#btnLux")
+const btnCerrar28 = document.querySelector("#btnCerrarLux")
+const modalLux = document.querySelector("#ventanaLux")
+
+btnLux.addEventListener("click",()=>{
+    modalLux.showModal();
+})
+
+btnCerrar28.addEventListener("click",()=>{
+    modalLux.close();
+})
